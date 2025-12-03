0c3bb97dda3c1c0fb1df877013242ebcbb74dd96
LessThan3door <tudorhadade@gmail.com>
2025-12-03 16:26:38 -0500

    Update team_builder.py

--- backend/team_builder.py
+++ backend/team_builder.py
@@ -529,7 +529,7 @@ class FEHTeamBuilder:
         return results
 
     
-        def suggest_captain_skill(self, team, datasets_with_skills=None):
+    def suggest_captain_skill(self, team, datasets_with_skills=None):
         """Pick captain skill based ONLY on historical usage of that captain."""
 
         datasets = datasets_with_skills if datasets_with_skills else self.datasets
